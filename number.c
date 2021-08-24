eaa03ff117752e0db882cc3e8701e71917293995
Dilshad <89437851+Hearthacker17@users.noreply.github.com>
2021-08-24 10:37:04 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+void main()
+{
+   int number;
+   printf("Enter number: ");
+   scanf("%d",&number);
+   switch(number)
+   {
+	case 1:
+	       printf("0\n");
+	       break;
+	case 2:
+	       printf("1\n");
+	       break;
+	case 3:
+	       printf("2\n");
+	       break;
+	case 4:
+	       printf("3\n");
+	       break;
+	case 5:
+	       printf("4\n");
+	       break;
+	case 6:
+	       printf("5\n");
+	       break;
+	case 7:
+	       printf("6\n");
+	       break;
+	case 8:
+	       printf("7\n");
+	       break;
+	case 9:
+	       printf("8\n");
+	       break;
+	case 10:
+	       printf("9\n");
+	       break;
+	case 11:
+	       printf("10\n");
+	       break;
+	default:
+	       printf("So\n");
+	       break;
+      }
+}
